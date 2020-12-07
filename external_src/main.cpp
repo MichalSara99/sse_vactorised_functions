6acad56eb8a8992012525447e0e5eeb527304b87
Michal Sara <michal.sara99@gmail.com>
2020-12-07 18:05:48 +0100

    ADDITION: 1) added tests on sqrpow() vectorised function, both SPFP and DPFP

--- external_src/main.cpp
+++ external_src/main.cpp
@@ -63,6 +63,8 @@ int main(int argc, char const* argv[]) {
 	// testBasicMinsSSEDouble();
 	// testBasicMaxsSSEFloat();
 	// testBasicMaxsSSEDouble();
+	// testBasicSqrpowSSEDouble();
+	// testBasicSqrpowSSEFloat();
 
 	// ==================================================
 
--- external_tests/basic_functions_et.h
+++ external_tests/basic_functions_et.h
@@ -775,7 +775,121 @@ void testBasicAbsSSEFloat() {
 }
 
 
+void testBasicSqrpowSSEDouble() {
 
+	int const n = 16;
+	std::size_t const align = 16;
+
+	double* x = sse_utility::aligned_alloc<double>(n, align);
+	double* res1 = sse_utility::aligned_alloc<double>(n, align);
+	double* res2 = sse_utility::aligned_alloc<double>(n, align);
+
+	// test some basic known values:
+
+	x[0] = 0.0;
+	x[1] = pi() / 2.0;
+	x[2] = pi();
+	x[3] = 3.0 * pi() / 2.0;
+	x[4] = 5.0 * pi() / 4.0;
+	x[5] = 2.0 * pi();
+	x[6] = 4.0 * pi();
+	x[7] = 3.0 * pi();
+	x[8] = 6.0 * pi() / 3.0;
+	x[9] = 2.0 * pi();
+	x[10] = pi() / 4.0;
+	x[11] = 7.0 * pi() / 4.0;
+	x[12] = 0.5;
+	x[13] = pi() / 3.0;
+	x[14] = 23.5;
+	x[15] = 4.0 * pi() / 3.0;
+
+	auto start_asm = std::chrono::system_clock::now();
+	bool rc1 = sqrpow_sse(x, n, res1);
+	auto end_asm = std::chrono::system_clock::now();
+	auto elapsed_asm = std::chrono::duration<double>(end_asm - start_asm).count();
+
+	auto start_cpp = std::chrono::system_clock::now();
+	for (int i = 0; i < n; ++i) {
+		res2[i] = pow(x[i],2);
+	}
+	auto end_cpp = std::chrono::system_clock::now();
+	auto elapsed_cpp = std::chrono::duration<double>(end_cpp - start_cpp).count();
+
+	SSE_ASSERT(rc1 == 1, "Failure in packed sqrpow SSE occured");
+
+	std::cout << "		C++				Assembly (SSE)			Difference\n";
+	std::cout << "=========================================================\n\n";
+	for (int i = 0; i < n; ++i) {
+		std::cout << i << " | " << res2[i];
+		std::cout << " | " << res1[i];
+		std::cout << " | " << (res1[i] - res2[i]) << "\n";
+	}
+	std::cout << "=========================================================\n\n";
+	std::cout << "\n" << "Elapsed (C++): " << elapsed_cpp;
+	std::cout << "\n" << "Elapsed (Assembly): " << elapsed_asm << "\n";
+
+	sse_utility::aligned_free(x);
+	sse_utility::aligned_free(res1);
+	sse_utility::aligned_free(res2);
+}
+
+void testBasicSqrpowSSEFloat() {
+
+	int const n = 16;
+	std::size_t const align = 16;
+
+	float* x = sse_utility::aligned_alloc<float>(n, align);
+	float* res1 = sse_utility::aligned_alloc<float>(n, align);
+	float* res2 = sse_utility::aligned_alloc<float>(n, align);
+
+	// test some basic known values:
+
+	x[0] = 0.0;
+	x[1] = pi() / 2.0;
+	x[2] = pi();
+	x[3] = 3.0 * pi() / 2.0;
+	x[4] = 5.0 * pi() / 4.0;
+	x[5] = 2.0 * pi();
+	x[6] = 4.0 * pi();
+	x[7] = 3.0 * pi();
+	x[8] = 6.0 * pi() / 3.0;
+	x[9] = 2.0 * pi();
+	x[10] = pi() / 4.0;
+	x[11] = 7.0 * pi() / 4.0;
+	x[12] = 0.5;
+	x[13] = pi() / 3.0;
+	x[14] = 23.5;
+	x[15] = 4.0 * pi() / 3.0;
+
+	auto start_asm = std::chrono::system_clock::now();
+	bool rc1 = sqrpow_sse(x, n, res1);
+	auto end_asm = std::chrono::system_clock::now();
+	auto elapsed_asm = std::chrono::duration<double>(end_asm - start_asm).count();
+
+	auto start_cpp = std::chrono::system_clock::now();
+	for (int i = 0; i < n; ++i) {
+		res2[i] = pow(x[i], 2);
+	}
+	auto end_cpp = std::chrono::system_clock::now();
+	auto elapsed_cpp = std::chrono::duration<double>(end_cpp - start_cpp).count();
+
+	SSE_ASSERT(rc1 == 1, "Failure in packed sqrpow SSE occured");
+
+	std::cout << "		C++				Assembly (SSE)			Difference\n";
+	std::cout << "=========================================================\n\n";
+	for (int i = 0; i < n; ++i) {
+		std::cout << i << " | " << res2[i];
+		std::cout << " | " << res1[i];
+		std::cout << " | " << (res1[i] - res2[i]) << "\n";
+	}
+	std::cout << "=========================================================\n\n";
+	std::cout << "\n" << "Elapsed (C++): " << elapsed_cpp;
+	std::cout << "\n" << "Elapsed (Assembly): " << elapsed_asm << "\n";
+
+	sse_utility::aligned_free(x);
+	sse_utility::aligned_free(res1);
+	sse_utility::aligned_free(res2);
+}
 
 
 
